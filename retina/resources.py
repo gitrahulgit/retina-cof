d15fee1539711554fd81fa524dfe508806428761
gitrahulgit <vrahul200023@gmail.com>
2024-02-03 00:56:08 +0530

    fix export

--- retina/resources.py
+++ retina/resources.py
@@ -12,6 +12,6 @@ class AnnotationResource(resources.ModelResource):
         export_order = ('patient_image', 'disease_type', 'x_cord', 'y_cord', 'radius', 'comment')
 
     def dehydrate_comment(self, annotation):
-        related_comment = Comment.objects.filter(patient_image=annotation.patient_image, disease_type="HVE")
-        if related_comment.exists():
+        related_comment = Comment.objects.filter(patient_image=annotation.patient_image)
+        if related_comment.exists() and annotation.disease_type == "HVE":
             return related_comment.first().comment
